9759213160418d4768124aea96b5a697cd050eb4
Jeremy Hsu <jeremyhsu1@gmail.com>
2021-05-23 19:13:18 -0700

    Import some process info

--- Activity/ContentView.swift
+++ Activity/ContentView.swift
@@ -8,25 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-  private var item1 = "Item 1"
-  private var item2 = "Item 2"
-  private var item3 = "Item 3"
-  
   var body: some View {
     VStack(alignment: .leading, spacing: 10) {
-      Text("First Header")
-        .font(.title)
-        .fontWeight(.semibold)
-      KeyValueRow(key: "Key1", value: "Value1")
-      KeyValueRow(key: "Key2", value: "Value2")
+      Group {
+        Text("OS Information")
+          .font(.title)
+          .fontWeight(.semibold)
+        KeyValueRow(
+          key: "Version",
+          value:ProcessInfo.processInfo.operatingSystemVersionString
+        )
+        Divider()
+      }
       
-      Divider()
-      
-      Text("Second Header")
-        .font(.title)
-        .fontWeight(.semibold)
-      KeyValueRow(key: "Key3", value: "Value3")
-      KeyValueRow(key: "Key4", value: "Value4")
+      Group {
+        Text("Device Information")
+          .font(.title)
+          .fontWeight(.semibold)
+        KeyValueRow(
+          key: "Processors",
+          value: String(ProcessInfo.processInfo.processorCount)
+        )
+        KeyValueRow(
+          key: "Currently Active Processors",
+          value: String(ProcessInfo.processInfo.activeProcessorCount)
+        )
+        KeyValueRow(
+          key: "Physical Memory",
+          value: String(ProcessInfo.processInfo.physicalMemory)
+        )
+        KeyValueRow(
+          key: "System Uptime",
+          value: String(ProcessInfo.processInfo.systemUptime)
+        )
+        Divider()
+      }
     }
     .padding()
   }
